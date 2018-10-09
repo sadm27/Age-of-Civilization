5ab96783e1804f38b35e133487697c59a1bf7c3a
Max Curren <j2curren@student.bridgew.edu>
2018-10-09 18:57:06 -0400

    Fixed merge issues (finally)
    Found and fixed the issue that prevented me from merging a branch to master.

---
+++ Assets/NPC_Actions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC_Actions : MonoBehaviour {
+
+    public float speed = .01f;
+    Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rigidbody.velocity = transform.forward * speed;
+	}
+}
