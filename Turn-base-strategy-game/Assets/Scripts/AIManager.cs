454a9ce96417c9c833fe3e519a58ae01d2ac6d06
sadm27 <jrapoza@student.bridgew.edu>
2018-12-03 22:22:50 -0500

    basic AI work
    working on basic movment AI

---
+++ Turn-base-strategy-game/Assets/Scripts/AIManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIManager : MonoBehaviour {
+
+    //public GameObject PlayerAI;
+    public Player AiPlayerScript;//adsada
+    //public GameObject GC;
+    public GameController GCScript;
+    //public GameObject MM;
+    public MouseManagerS MMS;
+
+
+
+    GameObject[] units;
+    GameObject unit;
+    Unit[] Uscripts;
+    Unit Uscript;
+
+    public string CurrPlayerCheck;
+
+    // Use this for initialization
+    void Start () {
+
+        AiPlayerScript = PlayerAI.GetComponent<Player>();
+        GCScript = GC.GetComponent<GameController>();
+        MMS = MM.GetComponent<MouseManagerS>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        CurrPlayerCheck = GC.GetCurrPlayer();
+
+    }
+
+
+    void AiMoveUnit()
+    {
+
+        if(CurrPlayerCheck == AiPlayerScript.tag.ToString())
+        {
+
+        }
+
+    }
+
+
+}
