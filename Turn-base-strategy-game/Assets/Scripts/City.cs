82ffc422312e4b92c3d44da6f619f03cea73f488
Max Curren <j2curren@student.bridgew.edu>
2018-12-17 19:25:10 -0500

    Cities and settlers reimplimented, and city FoW added
    Reimplimented cities and settlers to work with the current branch. Also added FoW to cities you found.

---
+++ Turn-base-strategy-game/Assets/Scripts/City.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class City : MonoBehaviour {
+
+    public string cityName = "New City";
+    public TextMesh CityNameLabel;
+
+	// Use this for initialization
+	void Start () {
+        CityNameLabel.text = cityName; //set label to city name
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+
+}
---
+++ Turn-base-strategy-game/Assets/Scripts/Settler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Settler : MonoBehaviour {
+
+    public GameObject settlerPanel;
+    public GameObject cityPrefab;
+    public Button settleCityBtn;
+    public InputField cityNameChoice;
+
+    MouseManagerS mouseMan;
+
+	// Use this for initialization
+	void Start () {
+        settleCityBtn.onClick.AddListener(settleCity);
+        mouseMan = GameObject.Find("MouseManager").GetComponent<MouseManagerS>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject == mouseMan.selectedUnit)
+        {
+            settlerPanel.SetActive(true);
+        }
+        else
+        {
+            if(settlerPanel.activeSelf == true)
+            {
+                settlerPanel.SetActive(false);
+            }
+        }
+    }
+
+    void settleCity()
+    {
+        if(this.gameObject == mouseMan.selectedUnit)
+        {
+            GameObject newCity = (GameObject)Instantiate(cityPrefab, this.gameObject.transform.position, Quaternion.Euler (180f, 0f, 0f));
+            newCity.transform.position = new Vector3(transform.position.x, transform.position.y, -0.5f);
+            City cityScript = newCity.GetComponent<City>();
+
+            if(cityNameChoice.text.ToString() != "")
+            {
+                cityScript.cityName = cityNameChoice.text.ToString();
+            }
+            else
+            {
+                //TODO: random name generator
+            }
+                
+            Destroy(this.gameObject);
+        }
+    } 
+
+
+}
